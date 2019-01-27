6fd1fd7368cb1a458e57af89ce4f550e8affe154
Helias <stefanoborzi32@gmail.com>
2019-01-27 22:41:33 +0100

    Improved logging_message

--- module/dev_functions.py
+++ module/dev_functions.py
@@ -4,26 +4,29 @@ with open('config/settings.yaml', 'r') as yaml_config:
     config_map = yaml.load(yaml_config)
 
 def logging_message(bot, update):
-	message_id = update.message.message_id #ID MESSAGGI\O
-	user = update.message.from_user # Restituisce un oggetto Telegram.User
-	chat = update.message.chat # Restituisce un oggetto Telegram.Chat
-	text = update.message.text #Restituisce il testo del messaggio
-	date = update.message.date #Restituisce la data dell'invio del messaggio
-	message = "\n___ID MESSAGE: " 	+ str(message_id) + "____\n" + \
-			  "___INFO USER___\n" + \
-			  "user_id:" 			+ str(user.id) + "\n" + \
-			  "user_name:" 			+ str(user.username) + "\n" + \
-			  "user_firstlastname:" + str(user.first_name) + " " + str(user.last_name) + "\n" + \
-			  "___INFO CHAT___\n" + \
-			  "chat_id:" 			+ str(chat.id) + "\n" + \
-			  "chat_type:" 			+ str(chat.type)+"\n" + "chat_title:" + str(chat.title) + "\n" + \
-			  "___TESTO___\n" + \
-			  "text:" 				+ str(text) + \
-			  "date:" 				+ str(date) + \
-			  "\n_____________\n"
+	try:
+		message_id = update.message.message_id #ID MESSAGGI\O
+		user = update.message.from_user # Restituisce un oggetto Telegram.User
+		chat = update.message.chat # Restituisce un oggetto Telegram.Chat
+		text = update.message.text #Restituisce il testo del messaggio
+		date = update.message.date #Restituisce la data dell'invio del messaggio
+		message = "\n___ID MESSAGE: " 	+ str(message_id) + "____\n" + \
+					"___INFO USER___\n" + \
+					"user_id:" 			+ str(user.id) + "\n" + \
+					"user_name:" 			+ str(user.username) + "\n" + \
+					"user_firstlastname:" + str(user.first_name) + " " + str(user.last_name) + "\n" + \
+					"___INFO CHAT___\n" + \
+					"chat_id:" 			+ str(chat.id) + "\n" + \
+					"chat_type:" 			+ str(chat.type)+"\n" + "chat_title:" + str(chat.title) + "\n" + \
+					"___TESTO___\n" + \
+					"text:" 				+ str(text) + \
+					"date:" 				+ str(date) + \
+					"\n_____________\n"
 
-	log_tmp = open("logs/logs.txt","a+")
-	log_tmp.write("\n"+message)
+		log_tmp = open("logs/logs.txt","a+")
+		log_tmp.write("\n"+message)
+	except:
+		pass
 
 # Devs Commands
 def give_chat_id(bot, update):
