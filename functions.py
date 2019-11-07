79be161fe609aa9f30a005439cd94f6ba873729c
BeastHunter19 <ciccio-peppe@live.com>
2019-11-07 19:58:01 +0100

    Updated functions.py with context based callbacks

--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # Telegram
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext
 from telegram.error import (TelegramError, Unauthorized, BadRequest, TimedOut, ChatMigrated, NetworkError)
 
 import yaml
@@ -14,17 +15,17 @@ with open('config/settings.yaml', 'r') as yaml_config:
 TOKEN = config_map["token"]
 
 # This function split the message into 2 or more messages in case of message length > 3000
-def send_message(bot, chaitd, messaggio):
+def send_message(context:CallbackContext, chaitd, messaggio):
     msg = ""
     righe = messaggio.split('\n')
     for riga in righe:
         if riga.strip() == "" and len(msg) > 3000:
-            bot.sendMessage(chat_id=chaitd, text=msg, parse_mode='Markdown')
+            context.bot.sendMessage(chat_id=chaitd, text=msg, parse_mode='Markdown')
             msg = ""
         else:
             msg += riga + "\n"
-    bot.sendMessage(chat_id=chaitd, text=msg, parse_mode='Markdown')
+    context.bot.sendMessage(chat_id=chaitd, text=msg, parse_mode='Markdown')
 
 # Commands
-def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Benvenuto! Questo bot è stato realizzato dagli studenti di Informatica al fine di suppotare gli studenti dell'Università di Catania! Per scoprire cosa puoi fare usa /help")
+def start(update: Update, context: CallbackContext):
+    context.bot.sendMessage(chat_id=update.message.chat_id, text="Benvenuto! Questo bot è stato realizzato dagli studenti di Informatica al fine di suppotare gli studenti dell'Università di Catania! Per scoprire cosa puoi fare usa /help")
