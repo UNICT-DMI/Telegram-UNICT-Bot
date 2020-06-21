c83bf7e35bd70ecaf8eb1e326d91b826928286ad
Lorenzo Catania <aegroto@protonmail.com>
2020-06-21 23:12:43 +0200

    implemented thread pool for scrapers scheduling, added configuration via command line arguments

--- build_notices_image.py
+++ build_notices_image.py
@@ -1,4 +1,6 @@
-import yaml, logging, traceback, bs4, os
+import yaml, logging, traceback, bs4, os, argparse
+
+from concurrent.futures import ThreadPoolExecutor
 
 from module.images_builder.department_scraper import DepartmentScraper
 from module.images_builder.threading import DepartmentScraperThread
@@ -8,33 +10,53 @@ def load_notices_urls():
     return config_map["notices_urls"]
 
 def main():
+    parser = argparse.ArgumentParser(description='UNICT-Bot notices images builder')
+    parser.add_argument('workers', type=int)
+    parser.add_argument('--logfile', nargs='+', help="Path to the file to print logs in")
+    parser.add_argument('--scrape-only', nargs='+', help="Scrape only given departments")
+    parser.add_argument('--exclude', nargs='+', help="Skip given departments while scraping")
+
+    args = parser.parse_args()
+
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO,
-        filename="logs.txt"
+        filename=args.logfile#"logs.txt"
     )
 
     logger = logging.getLogger(__name__)
 
     notices_urls = load_notices_urls()
 
+    executor = ThreadPoolExecutor(max_workers = args.workers)
+
     scrapers = []
+    futures = []
 
     for department_key in notices_urls:
+        if args.scrape_only and department_key not in args.scrape_only:
+            continue
+
+        if args.exclude and department_key in args.exclude:
+            continue
+
         logger.info("Creating scraper for departiment '{}'".format(department_key))
 
         scraper = DepartmentScraper(department_key, notices_urls[department_key])
-        scraper_thread = DepartmentScraperThread(scraper)
+        # scraper_thread = DepartmentScraperThread(scraper)
 
-        scrapers.append(scraper_thread)
-
-    for scraper in scrapers:
-        scraper.start()
-
-        # return # TODO: Remove to cycle all departments
+        # scrapers.append(scraper_thread)
+        scrapers.append(scraper)
 
     for scraper in scrapers:
-        scraper.join()
+        future = executor.submit(scraper.run)
+
+        futures.append(future)
+
+    for future in futures:
+        future.result()
+
+        # No result is expected (right now)
 
 if __name__ == "__main__":
     main()
--- module/images_builder/department_scraper.py
+++ module/images_builder/department_scraper.py
@@ -1,16 +1,32 @@
-import logging, bs4, traceback, os
+import requests, logging, bs4, traceback, os, time
 
 from module.scraper_notices import request_page_content, get_links
 
 class DepartmentScraper():
     def __init__(self, department_key, department_data):
-        self.department_key = department_key
-        self.department_data = department_data
+        self.__department_key = department_key
+        self.__department_data = department_data
 
-        self.logger = logging.getLogger("'{}' department scraper".format(department_key))
+        self.__logger = logging.getLogger("'{}' department scraper".format(self.__department_key))
 
-    def scrape_links_from_page(self, file_writer, page_id, page_url):
-        page_content = request_page_content(page_url)
+    def __str__(self):
+        return "Department '{}' scraper".format(self.__department_key)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __scrape_links_from_page(self, file_writer, page_id, page_url, exception_sleep=5):
+        while True:
+            try:
+                page_content = request_page_content(page_url)
+                break
+            except requests.exceptions.ConnectionError:
+                logging.warn("Connection error while requesting page content, sleeping and retrying...")
+
+                traceback.print_exc()
+
+                time.sleep(exception_sleep)
+
         links = get_links(page_id, page_content)
 
         # self.logger.info(links)
@@ -18,24 +34,24 @@ class DepartmentScraper():
         for link in links:
             file_writer.write("{}\n".format(str(link)))
 
-    def scrape_archive_pages(self, file_writer, page_id, page_url, max_archive_page_index):
+    def __scrape_archive_pages(self, file_writer, page_id, page_url, max_archive_page_index):
         archive_page_index = 1
 
         while archive_page_index <= max_archive_page_index:
             archive_page_url = "{}/archivio?page={}".format(page_url, archive_page_index)
 
-            self.logger.info("Scraping archive's page {}...".format(archive_page_url))
+            self.__logger.info("Scraping archive's page {}...".format(archive_page_url))
 
-            self.scrape_links_from_page(file_writer, page_id, archive_page_url)
+            self.__scrape_links_from_page(file_writer, page_id, archive_page_url)
 
             archive_page_index += 1
 
-    def scrape_single_page(self, file_writer, page_id, page_url):
-        self.logger.info("Scraping single page {}...".format(page_url))
+    def __scrape_single_page(self, file_writer, page_id, page_url):
+        self.__logger.info("Scraping single page {}...".format(page_url))
 
-        self.scrape_links_from_page(file_writer, page_id, page_url)
+        self.__scrape_links_from_page(file_writer, page_id, page_url)
 
-    def get_last_page_in_archive(self, page_url):
+    def __get_last_page_in_archive(self, page_url):
         try:
             first_page_content = request_page_content("{}/archivio".format(page_url))
             soup = bs4.BeautifulSoup(first_page_content, 'html.parser')
@@ -44,45 +60,53 @@ class DepartmentScraper():
 
             return int(result['href'].split("=")[-1])
         except:
-            self.logger.warning("Couldn't scrape last page, assuming there's no archive and only a single page is present")
+            self.__logger.warning("Couldn't scrape last page, assuming there's no archive and only a single page is present")
 
             # traceback.print_exc()
 
             return 0
 
-    def run(self):
-        self.logger.info("Building a complete image for department '{}'...".format(self.department_key))
+    def __escape_filename_chars(self, filename):
+        filename = filename.replace("/", " ")
 
-        department_root_folder = "images/{}".format(self.department_key)
+        return filename
+
+    def run(self):
+        self.__logger.info("Building a complete image for department '{}'...".format(self.__department_key))
+
+        department_root_folder = "images/{}".format(self.__escape_filename_chars(self.__department_key))
 
         try:
             os.mkdir(department_root_folder)
         except FileExistsError:
-            self.logger.warning("Department's folder exists, resulting images may be dirty and/or invalid.")
+            self.__logger.warning("Department's folder exists, resulting images may be dirty and/or invalid.")
 
-        for page_id, page_data in self.department_data["pages"].items():
-            self.logger.info("Scraping page '{}'...".format(page_id))
+        for page_id, page_data in self.__department_data["pages"].items():
+            self.__logger.info("Scraping page '{}'...".format(page_id))
 
-            image_file_path = "{}/{}_avvisi.dat".format(department_root_folder, page_id)
+            image_file_path = "{}/{}_avvisi.dat".format(
+                department_root_folder,
+                self.__escape_filename_chars(page_id)
+            )
 
             if os.path.exists(image_file_path):
-                self.logger.info("Image file exists already, skipping")
+                self.__logger.info("Image file exists already, skipping")
                 continue
 
             file_writer = open(image_file_path, 'w')
 
             for page_url in page_data["urls"]:
-                max_archive_page_index = self.get_last_page_in_archive(page_url)
+                max_archive_page_index = self.__get_last_page_in_archive(page_url)
 
-                self.logger.info("Last page: {}".format(max_archive_page_index))
+                self.__logger.info("Last page: {}".format(max_archive_page_index))
 
                 try:
                     if max_archive_page_index > 0:
-                        self.scrape_archive_pages(file_writer, page_id, page_url, max_archive_page_index)
+                        self.__scrape_archive_pages(file_writer, page_id, page_url, max_archive_page_index)
                     else:
-                        self.scrape_single_page(file_writer, page_id, page_url)
+                        self.__scrape_single_page(file_writer, page_id, page_url)
                 except:
-                    self.logger.warning("Unhandled exception while scraping")
+                    self.__logger.warning("Unhandled exception while scraping")
                     traceback.print_exc()
 
                     return
